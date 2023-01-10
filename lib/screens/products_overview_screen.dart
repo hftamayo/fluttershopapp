59dedcc97e1932c3c8709dfef640f9cb8ae4573e
hftamayo <hftamayo@gmail.com>
2023-01-10 08:17:59 -0600

    adding support to the product item class

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/product.dart';
+import '../widgets/product_item.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   final List<Product> loadedProducts = [
@@ -46,8 +47,14 @@ class ProductsOverviewScreen extends StatelessWidget {
       body: GridView.builder(
         padding: const EdgeInsets.all(10.0),
         itemCount: loadedProducts.length,
-        itemBuilder: (ctx, i) => Container(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(),
+        itemBuilder: (ctx, i) => ProductItem(loadedProducts[i].id,
+            loadedProducts[i].title, loadedProducts[i].imageUrl),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
       ),
     );
   }
